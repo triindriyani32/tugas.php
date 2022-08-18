058a783c5133f6d2499035a0d71ebe6202f08b3a
triindriyani32 <109188752+triindriyani32@users.noreply.github.com>
2022-08-17 17:47:09 -0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Tugas kelipatan </title>
+</head>
+<body>
+   <p> -Menampilkan perulangan bilangan ganjil 100 -> 0 </p>
+   <?php
+   $i = 99;
+   while ($i >= 1) {
+    echo "$i";
+    echo "<br/>";
+    $i-=2;
+   }
+   ?>
+   <p> -menampilkan bilangan kelipatan 3 maksimal 10 bilangan- </p>
+   
+</body>
+</html>
